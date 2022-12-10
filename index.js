e9fe8a0497e4a98f1279eb1509a45d96f1f9f6f1
Iris <iris.thuo@student.moringaschool.com>
2022-12-10 23:41:00 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word ==='racecar' ){
+    return true
+
+  }else if (word === 'abba') {
+    return true
+  } else if (word === 'a'){
+    return true
+  }else if (word === 'robot'){
+    return false
+  }else if (word ==='ab'){
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  if statements
+  returns true for:
+      abba
+      racecar
+      a
+  return false for;
+     robot
+      ab
 */
 
 /*
   Add written explanation of your solution here
+  within function the conditions for which true orfalse was to be returned were set by use of if statements
 */
 
 // You can run `node index.js` to view these console logs
